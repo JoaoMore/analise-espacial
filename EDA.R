bff3fcd368e9e30c9940844de4e3a4dcdb325895
João Moraes <49118537+JoaoMore@users.noreply.github.com>
2022-03-16 11:38:00 -0300

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -1,7 +1,14 @@
 library(tidyverse)
 library(gghighlight)
+theme_set(theme_minimal())
+update_geom_defaults("boxplot",   list(fill = "blue"))
 
 
 # Infectados por 100k -----------------------------------------------------
 
-infec <- read_csv('out/data/casos100k.csv')
+infec <- read_csv("out/data/casos100k.csv", 
+                  locale = locale(decimal_mark = ","))
+
+infec %>% 
+  ggplot(aes(x = factor(ano), y = casos100k)) +
+  geom_boxplot() 
