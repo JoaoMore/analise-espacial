8189f3a6b0e8493a718a5b82baafa765a8712240
João Moraes <49118537+JoaoMore@users.noreply.github.com>
2022-03-16 12:15:24 -0300

    update

--- scr/EDA.R
+++ scr/EDA.R
@@ -2,9 +2,14 @@ library(tidyverse)
 library(gghighlight)
 theme_set(theme_minimal())
 color <- 'dodgerblue3'
+update_geom_defaults("line",   list(color = color))
 update_geom_defaults("boxplot",   list(fill = color))
 update_geom_defaults("col",   list(fill = color))
 update_geom_defaults("bar",   list(fill = color, color = 'gray80'))
+
+altura <- 4.5
+comprimento <- 2.5
+escala <- 2
 
 # Infectados por 100k -----------------------------------------------------
 
@@ -17,7 +22,8 @@ infec %>%
   geom_histogram() +
   labs(title = 'Histograma de casos por 100 mil habitantes')
 
-ggsave(filename = 'histograma_100k.png', width = 4.5, height = 2.5, scale = 2, path = 'out/plots/')
+ggsave(width = altura, height = comprimento, scale = escala, 
+       filename = 'histograma_100k.png', path = 'out/plots/')
 
 ## boxplot ----
 infec %>% 
@@ -26,4 +32,17 @@ infec %>%
   labs(x = NULL, y = 'Casos por 100 mil habitantes', 
        title = 'Boxplot de casos por 100 mil habitantes, por ano')
 
-ggsave(filename = 'boxplot_100k.png', width = 4.5, height = 2.5, scale = 2, path = 'out/plots/')
+ggsave(width = altura, height = comprimento, scale = escala, 
+       filename = 'boxplot_100k.png', path = 'out/plots/')
+
+## Evolução do numero de casos ----
+
+infec %>% 
+  ggplot(aes(ano, casos100k, col = municipio)) +
+  geom_line() +
+  gghighlight(mean(casos100k) > 100) +
+  labs(title = 'Evolução do número de casos por 100 mil habitantes', 
+       subtitle = 'Municipios com média acima de 100', x = NULL, y = 'Casos/100 mil habitantes')
+
+ggsave(width = altura, height = comprimento, scale = escala, 
+       filename = 'evolução_100k.png', path = 'out/plots/')
